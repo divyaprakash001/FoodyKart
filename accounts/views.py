091509dda404f8a3d4bf0c7bf04952a84ad69e65
Divya Prakash <divyaprakashara09@gmail.com>
2024-08-08 11:06:30 +0530

    vendor registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from .forms import UserForm
 from vendor.forms import VendorForm
-from .models import User
+from .models import User,UserProfile
+from vendor.models import Vendor
 from django.contrib import messages
 
 # Create your views here.
@@ -41,12 +42,30 @@ def registerUser(request):
   return render(request,"accounts/registerUser.html",context)
 
 def registerVendor(request):
-  if request.method=='post':
+  if request.method=='POST':
     form = UserForm(request.POST)
-    v_form = VendorForm(request.POST)
-    if form.is_valid():
-      messages.success(request, "Your account has been registered successfully!")
-      return redirect('registerUser')
+    v_form = VendorForm(request.POST,request.FILES)
+    if form.is_valid() and v_form.is_valid():
+      first_name = form.cleaned_data['first_name']
+      last_name = form.cleaned_data['last_name']
+      username = form.cleaned_data['username']
+      email = form.cleaned_data['email']
+      password = form.cleaned_data['password']
+
+      # create user as per our definition
+      user = User.objects.create_user(first_name=first_name,last_name=last_name,username=username,email=email,password=password)
+      user.role = User.VENDOR
+      user.save()
+      print("user created")
+      # form for vendor
+      vendor = v_form.save(commit=False)
+      vendor.user = user
+      user_profile = UserProfile.objects.get(user=user)
+      vendor.user_profile = user_profile
+      vendor.save()
+      # vendor created
+      messages.success(request, "Your account has been registered successfully! Please wait for the approval.")
+      return redirect('registerVendor')
     else:
       print("Invalid form")
       print(form.errors)
--- vendor/admin.py
+++ vendor/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from .models import Vendor
+
 # Register your models here.
 class VendorAdmin(admin.ModelAdmin):
-  list_display = ['vendor_name','user','is_approved']
+  list_display = ['user','vendor_name','is_approved','created_at']
   list_filter = ['is_approved','created_at','modified_at']
+  list_display_links = ['user','vendor_name']
 
 
 admin.site.register(Vendor,VendorAdmin)
