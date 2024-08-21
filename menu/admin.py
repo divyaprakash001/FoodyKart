11ff41c7b510663a572bb20809c274d754dc2a9b
Divya Prakash <divyaprakashara09@gmail.com>
2024-08-22 00:31:24 +0530

    Creating category models, foodItem models and crud operations on category

---
+++ menu/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from .models import Category, FoodItem
+
+# Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+  prepopulated_fields = {'slug': ('category_name',)}
+  list_display=['category_name','vendor','updated_at']
+  list_display_links = ['vendor','category_name']
+  search_fields = ['category_name','vendor__vendor_name']
+  list_filter=['created_at','updated_at']
+
+
+
+class FoodItemAdmin(admin.ModelAdmin):
+  list_display=['food_title','category','vendor','price','is_available','updated_at']
+  list_display_links = ['vendor','category','price']
+  list_editable = ['is_available']
+  search_fields = ['category__category_name','food_title','vendor__vendor_name','price']
+
+  prepopulated_fields = {'slug': ('food_title',)}
+
+
+admin.site.register(Category,CategoryAdmin)
+admin.site.register(FoodItem,FoodItemAdmin)
--- vendor/views.py
+++ vendor/views.py
@@ -7,6 +7,12 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.core.exceptions import PermissionDenied
 from accounts.views import check_role_vendor
+from menu.models import Category,FoodItem
+from .utils import get_vendor
+from menu.forms import CategoryForm
+from django.template.defaultfilters import slugify
+
+
 
 
 # Create your views here.
@@ -37,3 +43,84 @@ def vprofile(request):
     'vendor':vendor
     }
   return render(request,'vendor/vprofile.html',context)
+
+@login_required(login_url='login')
+@user_passes_test(check_role_vendor)
+def menu_builder(request):
+  vendor = get_vendor(request)
+  categories = Category.objects.filter(vendor=vendor).order_by("created_at")
+
+  context={
+    'categories':categories,
+  }
+  return render(request,"vendor/menu_builder.html", context)
+
+@login_required(login_url='login')
+@user_passes_test(check_role_vendor)
+def fooditems_by_category(request,pk=None):
+  vendor = vendor = get_vendor(request)
+  category = get_object_or_404(Category, pk = pk)
+  fooditems = FoodItem.objects.filter(vendor=vendor,category=category)
+  print(fooditems)
+  context = {
+    'category':category,
+    'fooditems':fooditems,
+  }
+  return render(request,"vendor/fooditems_by_category.html",context)
+
+@login_required(login_url='login')
+@user_passes_test(check_role_vendor)
+def add_category(request):
+  if request.method == 'POST':
+    form = CategoryForm(request.POST)
+    if form.is_valid():
+      category_name = form.cleaned_data['category_name']
+      category  = form.save(commit=False)
+      category.vendor= get_vendor(request)
+      category.slug = slugify(category_name)
+      form.save()
+      messages.success(request,"Category Added Successfully.")
+      return redirect("menu_builder")
+    else:
+      print(form.errors)
+  else:
+    form = CategoryForm()
+  context={
+    'form':form
+  }
+  return render(request,"vendor/add_category.html",context)
+
+
+@login_required(login_url='login')
+@user_passes_test(check_role_vendor)
+def edit_category(request,id=None):
+  category  = get_object_or_404(Category,pk=id)
+  if request.method == 'POST':
+    form = CategoryForm(request.POST,instance=category)
+    if form.is_valid():
+      category_name = form.cleaned_data['category_name']
+      category  = form.save(commit=False)
+      category.vendor= get_vendor(request)
+      category.slug = slugify(category_name)
+      form.save()
+      messages.success(request,"Category Updated Successfully.")
+      return redirect("menu_builder")
+    else:
+      print(form.errors)
+  else:
+    form = CategoryForm(instance=category)
+  context={
+    'form':form,
+    'category':category
+  }
+  return render(request,"vendor/edit_category.html",context)
+  
+
+
+
+def delete_category(request,id=None):
+  vendor = vendor = get_vendor(request)
+  category = get_object_or_404(Category, pk = id)
+  category.delete()
+  messages.success(request,"Category Deleted Successfully.")
+  return redirect("menu_builder")
