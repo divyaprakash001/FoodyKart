0b4839460db399143a3b04ad282a4bc7ba484b74
Divya Prakash <divyaprakashara09@gmail.com>
2024-10-27 21:01:07 +0530

    order placed done with receipt

--- orders/admin.py
+++ orders/admin.py
@@ -2,6 +2,22 @@ from django.contrib import admin
 from .models import Payment,Order,OrderedFood
 
 # Register your models here.
-admin.site.register(Payment)
-admin.site.register(Order)
-admin.site.register(OrderedFood)
+class OrderedFoodInline(admin.TabularInline):
+  model=OrderedFood
+  readonly_fields = ['order','payment','user','fooditem','quantity','price','amount']
+  extra=0
+
+class OrderAdmin(admin.ModelAdmin):
+  list_display = ['order_number','payment','user','email','total','is_ordered','status','updated_at']
+  inlines = [OrderedFoodInline]
+
+class OrderedFoodAdmin(admin.ModelAdmin):
+  list_display = ['fooditem','quantity','price','amount','user','payment','order','updated_at']
+
+class PaymentAdmin(admin.ModelAdmin):
+  list_display = ['transaction_id','user','payment_method','amount','status','created_at']
+
+
+admin.site.register(Payment,PaymentAdmin)
+admin.site.register(Order,OrderAdmin)
+admin.site.register(OrderedFood,OrderedFoodAdmin)
